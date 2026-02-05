659fd2ef396cdebd5d533411a951e4d04d2a0764
bradapc <brad.cocar@gmail.com>
2026-02-05 13:30:07 -0800

    Create registration route, controller, db

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//For parsing JSON bodies
+app.use(express.json());
+
+app.use('/register', require('./routes/register'));
+
 app.get('/', (req, res) => {
     res.json({
         "version": "1.0.0",
