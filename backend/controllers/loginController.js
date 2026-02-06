66193ee717af169fa767b34e243c41796efa7abd
bradapc <brad.cocar@gmail.com>
2026-02-05 17:42:23 -0800

    Add jwt generation on valid login

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,5 +1,6 @@
 const {connectToDb} = require('../db/connection');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
     if (!req.body) {
@@ -20,11 +21,18 @@ const handleLogin = async (req, res) => {
     }
 
     const match = await bcrypt.compare(password, dbResult.hashedPassword);
-    if (match) {
-        return res.status(201).json({'message': 'Match'});   
-    } else {
+    if (!match) {
         return res.status(401).json({'message': 'No match'})
     }
+
+    const payload = {
+        userId: dbResult.id,
+        role: dbResult.role
+    }
+    const JWT_SECRET = process.env.JWT_SECRET;
+    const accessToken = jwt.sign(payload, JWT_SECRET, {expiresIn: '15m'});
+
+    return res.status(201).json(accessToken);
 };
 
 module.exports = {handleLogin}
