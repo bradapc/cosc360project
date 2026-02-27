1e9e065245e083aa3ffc499af4a05cc1d19a6737
VDhillon05 <vinaydhillon2005@gmail.com>
2026-02-27 10:40:37 -0800

    Login Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       Hello COSC360Project!
+      <Login />
     </div>
+
   );
 }
 
