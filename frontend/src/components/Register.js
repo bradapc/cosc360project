b080805e3f92d38256c426f98b0e27133251d672
bradc <brad.cocar@gmail.com>
2026-02-27 16:03:13 -0800

    Add more fields to register form

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -3,11 +3,26 @@ import '../css/Auth.css'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+ 
   return (
     <div className="auth-div">
         <form method="post" className="auth-form">
           <h2>Sign up</h2>
           <p>Lets find a job right for you</p>
+
+          <div>
+            <input className="auth-input-box" type="text" placeholder = "First Name" aria-label="first-name"/>
+          </div>
+
+          <div>
+            <input className="auth-input-box" type="text" placeholder = "Last Name" aria-label="last-name"/>
+          </div>
+
+          <div id="dob">
+            <img src="https://uxwing.com/wp-content/themes/uxwing/download/festival-culture-religion/birthday-icon.png" />
+            <input className="auth-input-box" type="date" id="bday" name="bday" />
+          </div>
+
           <div>
             <input className="auth-input-box" type="text" placeholder = "Username" aria-label="username"/>
           </div>
@@ -18,6 +33,10 @@ const Register = () => {
 
           <div>
             <input className="auth-input-box" type="password" id="password" name="password" placeholder = "Password" aria-label="password"/>
+          </div>
+
+          <div>
+            <input className="auth-input-box" type="password" id="confirm-password" name="confirm-password" placeholder="Confirm Password" aria-label="confirm-password" />
           </div>
 
           <button type="submit">Register</button>
