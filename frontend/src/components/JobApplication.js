35b281ee860b3b326c84c3a93572d67dc894707f
bradc <brad.cocar@gmail.com>
2026-03-07 21:42:12 -0800

    Start application frontend

--- frontend/src/components/JobApplication.js
+++ frontend/src/components/JobApplication.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import {useParams} from "react-router-dom";
 
 const JobApplication = () => {
+    const [job, setJob] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
+    const {id} = useParams();
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/jobs/${id}`, {
+                    headers: { "Content-Type": "application/json" }
+                });
+                if (!response.ok) {
+                    throw new Error(`Could not fetch job ${id}`);
+                }
+                const json = await response.json();
+                setJob(json);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchJob();
+    }, [id]);
   return (
     <div>
-      
+            {job && 
+            <div className="job-application-summary">
+                <h1>Apply for: {job.title}</h1>
+                <p>{job.company}</p>
+                <p>${job.salaryRange.min} - ${job.salaryRange.max} / year</p>
+                <p>Key Responsibilities:</p>
+                <ul>
+                    {job.responsibilities.map((resp, idx) => (
+                        <li key={idx}>{resp}</li>
+                    ))}
+                </ul>
+            </div>
+
+        }
+      <form className="job-application-form">
+
+      </form>
     </div>
   )
 }
