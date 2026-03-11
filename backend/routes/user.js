70d7e9252c80911f40c6e64dd0f0db36e53fb418
JulianMohamed12 <julianmmohamed@gmail.com>
2026-03-10 18:10:30 -0700

    added user controller and router

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+
+const verifyJWT = require('../middleware/verifyJWT');
+
+router.get('/:id', usersController.handleGetUserById);
+router.post('/', verifyJWT, usersController.handlePostUser);
+router.patch('/:id', verifyJWT, usersController.handleUpdateUser);
+router.delete('/:id', verifyJWT, usersController.handleDeleteUser);
+
+module.exports = router;
